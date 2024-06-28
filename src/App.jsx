1dfc84add923c797862b062584346af4de3cfd4d
Mykyta Opanasiuk <nikitaopanassiuk@outlook.com>
2024-06-29 02:08:50 +0300

    Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Header from "./components/Header/Header";
 import Forecasts from "./components/Forecasts/Forecasts";
 import Footer from "./components/Footer/Footer";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <>
+    <Fragment>
       <Header />
       <main className="main">
         <Forecasts />
       </main>
       <Footer />
-    </>
+    </Fragment>
   );
 }
 
